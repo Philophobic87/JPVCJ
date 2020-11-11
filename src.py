2d73728dd559a82eea8feaa28973416c73e179a7
Philophobic87 <73677984+Philophobic87@users.noreply.github.com>
2020-11-11 12:09:22 -0500

    Update src.py

--- src.py
+++ src.py
@@ -1,4 +1,4 @@
-# User inputs a Jpanaese verb infinitive, outputs verb definition, Kanji characterization, as well as the present, negative, -te and -ta conjugation forms
+# User inputs a Japanaese verb infinitive, outputs verb definition, Kanji characterization, as well as the present, negative, -te and -ta conjugation forms
 
 import re
 from os import system, name 
